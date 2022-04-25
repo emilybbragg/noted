43372df0fcdcae1c92676a987809a48773b65d63
Emily <emilybribragg@gmail.com>
2022-04-25 15:18:11 -0400

    Initiate post request

--- src/Components/NotesList.js
+++ src/Components/NotesList.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Notes from "./Notes";
 
 function NotesList() {
-    //fetch the data using useEffect hook
     const [notes, setNotes] = useState([]);
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
@@ -17,7 +16,7 @@ function NotesList() {
     console.log(notes)
 
     const allNotes = notes.map((note) => {
-        return <Notes key={note.id} notes={notes} setNotes={setNotes}/>
+        return <Notes key={notes.id} notes={notes} setNotes={setNotes} handleDeleteClick={handleDeleteClick}/>
       });
 
       function handleDeleteNote(deletedNote) {
@@ -25,30 +24,49 @@ function NotesList() {
         setNotes(updatedNotes)
       }
     
-      function handleDeleteClick(listing) {
-        fetch(`http://localhost:6001/listings/${listing.id}`, {
+      function handleDeleteClick(notes) {
+        fetch(`http://localhost:3001/notes/${notes.id}`, {
           method: "DELETE",
         })
           .then((r) => r.json())
           .then(() => handleDeleteNote(notes))
-            // 1. find the listing in all listings (based on listing.id) - .filter
-            // 2. remove it from all listings by setting state
       }
 
-    return (
-    <main>
-         <ul className="notes">
-            {allNotes}
-         <form>
-              <label htmlFor="name-input">Name:</label>
-              <input id="name-input" type="text" value={name}/>
-             <label htmlFor="description-input">Description:</label>
-              <input id="description-input" type="text" value={description}/>
-             <input type="submit" />
-        </form>
-      </ul>
-    </main>
+      function handleSubmit(e) {
+        e.preventDefault();
+        const notesData = {
+          description: description,
+          name: name,
+        };
+        fetch("http://localhost:3001/notes", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(notesData),
+        })
+          .then((r) => r.json())
+          .then((newNotes) => {
+            console.log(newNotes)
+            const allNotesWithNew = [...notes, newNotes]
+            setNotes(allNotesWithNew)
+          })
+        }
 
-    )
-}
+    return (
+        <main>
+            <ul className="cards">
+            {allNotes}
+             <form onSubmit={handleSubmit}>
+                <label htmlFor="name-input">Name:</label>
+                 <input id="name-input" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+                 <label htmlFor="description-input">Description:</label>
+                <input id="description-input" type="text" value={description} onChange={(e) => setDescription(e.target.value)}/>
+                <input type="submit" />
+            </form>
+            </ul>
+         </main>
+          );
+        }
+        
 export default NotesList;
