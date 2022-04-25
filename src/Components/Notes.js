19a1df122aee14785e825607bdb510fcfa3dcf81
Emily <emilybribragg@gmail.com>
2022-04-25 14:51:33 -0400

    wrote useEffect hook

---
+++ src/Components/Notes.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Notes( {notes} ) {
+    
+
+}
+
+export default Notes;
---
+++ src/Components/NotesList.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import Notes from "./Notes";
+
+function NotesList() {
+    //fetch the data using useEffect hook
+    const [notes, setNotes] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:3000/notes")
+            .then((r) => r.json())
+            .then((notes) => {
+            setNotes(notes)
+             })
+    }, [])
+    console.log(notes)
+
+    const allNotes = notes.map((note) => {
+        return <Notes key={note.id} notes={notes} setNotes={setNotes}/>
+      });
+
+      function handleDeleteNote(deletedNote) {
+        const updatedNotes = notes.filter((note) => note.id !== deletedNote.id)
+        setNotes(updatedNotes)
+      }
+    
+      function handleDeleteClick(listing) {
+        fetch(`http://localhost:6001/listings/${listing.id}`, {
+          method: "DELETE",
+        })
+          .then((r) => r.json())
+          .then(() => handleDeleteListing(listing))
+            // 1. find the listing in all listings (based on listing.id) - .filter
+            // 2. remove it from all listings by setting state
+      }
+
+    return (
+    <main>
+         <ul className="notes">
+            {allNotes}
+         <form onSubmit={handleSubmit}>
+              <label htmlFor="name-input">Name:</label>
+              <input id="name-input" type="text" value={name}/>
+             <label htmlFor="description-input">Description:</label>
+              <input id="description-input" type="text" value={description}/>
+             <input type="submit" />
+        </form>
+      </ul>
+    </main>
+
+    )
+}
+export default NotesList;
