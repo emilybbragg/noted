0f42a384f89f54c48e732f66961808714cf853e7
Emily <emilybribragg@gmail.com>
2022-04-26 11:49:00 -0400

    Add greeting function

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import moment from 'moment';
 
 
 function Home () {
-    /*const current = new Date();
-    console.log(current)
-    return current.toLocaleTimeString('en-US')
-    */
+    //const current = new Date();
+    //console.log(current);
+    //return current.toLocaleTimeString('en-US');
+    const [date, setDate] = useState(new Date())
+    const changeDate = (e) => {
+      setDate(e)
+    }
+
+    const [greet, setGreet] = useState('Evening')
+    const findGreet = () => {
+        const hours = new Date().getHours();
+        if(hours === 0 || hours < 12) return setGreet('Morning');
+        if(hours === 1 || hours < 17) return setGreet('Afternoon')
+    }
+    useEffect(() => {
+        findGreet();
+    }, [])
+
+    return (
+      <div>
+      <div>{`Good ${greet}!`}</div>
+        <Calendar 
+        value={date}
+        onChange={changeDate}
+        />
+      <p>Current selected date is <b>{moment(date).format('MMMM Do YYYY')}</b></p>
+      </div>
+    )
+  }
+
+export default Home;
+
+
+
+ /*
     const getGreetingTime = (currentTime) => {
         if (!currentTime || !currentTime.isValid()) {
           return "Hello";
@@ -34,6 +66,4 @@ function Home () {
             <Calendar />
           </main>
       )
-}
-
-export default Home;
+      */
