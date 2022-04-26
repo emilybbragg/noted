cb9cf59bc0286074e5991ee2c24ab96e65960c0a
Emily <emilybribragg@gmail.com>
2022-04-26 09:37:56 -0400

    Initiate NavLinks

---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+  <div className="navbar">
+    <NavLink to ="/">Home</NavLink>
+    <NavLink to ="/reminderlist">Reminders</NavLink>
+    <NavLink to ="/noteslist">Notes</NavLink>
+  </div>
+  )}
+
+
+export default NavBar;
