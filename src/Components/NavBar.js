c3e1c717fe996ef2fb4008b10e6a30b91da1f113
Emily <emilybribragg@gmail.com>
2022-04-29 01:37:42 -0400

    Add classnames for CSS

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 
 function NavBar() {
   return (
-  <div className="navbar">
-    <NavLink to ="/">Home</NavLink>
-    <NavLink to ="/reminderlist">Reminders</NavLink>
-    <NavLink to ="/notelist">Notes</NavLink>
+  <div className="navigation">
+    <Navbar bg="light" variant="light" className="navBar">
+      <div className="navContainer">
+      <div className="title">Noted.</div>
+      <Nav className="me-auto">
+        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+        <Nav.Link as={NavLink} to="/reminderlist">Reminders</Nav.Link>
+        <Nav.Link as={NavLink} to="/notelist">Notes</Nav.Link>
+      </Nav>
+        </div>
+    </Navbar>
   </div>
-  )}
-
+  )
+}
 
 export default NavBar;
