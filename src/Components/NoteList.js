738ba5cfddd3a9ebf5566f07e42146d4965bab79
Emily <emilybribragg@gmail.com>
2022-04-29 01:38:18 -0400

    Add classnames for CSS

--- src/Components/NoteList.js
+++ src/Components/NoteList.js
@@ -2,75 +2,81 @@ import React, { useEffect, useState } from "react";
 import Note from "./Note";
 
 function NoteList() {
-    const [notes, setNotes] = useState([]);
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
 
-    useEffect(() => {
-        handleGetNotes()
-    }, [])
+  useEffect(() => {
+    handleGetNotes()
+  }, [])
 
-    const handleGetNotes = () => {
-        return fetch("http://localhost:3000/notes")
-            .then((r) => r.json())
-            .then((notes) => {
-                setNotes(notes)
-            })
-    }
-    console.log(notes)
+  const handleGetNotes = () => {
+    return fetch("http://localhost:3000/notes")
+      .then((r) => r.json())
+      .then((notes) => {
+        setNotes(notes)
+      })
+  }
 
-    const allNotes = notes.map((note) => {
-        return <Note key={note.id} note={note} handleDeleteClick={handleDeleteClick}/>
-    });
+  const allNotes = notes.map((note) => {
+    return <Note key={note.id} note={note} handleDeleteClick={handleDeleteClick}/>
+  });
 
-      function handleDeleteNote(deletedNote) {
-        const updatedNotes = notes.filter((note) => note.id !== deletedNote.id)
-        setNotes(updatedNotes)
-      }
+  function handleDeleteNote(deletedNote) {
+    const updatedNotes = notes.filter((note) => note.id !== deletedNote.id)
+    setNotes(updatedNotes)
+  }
     
-      function handleDeleteClick(notes) {
-        fetch(`http://localhost:3000/notes/${notes.id}`, {
-          method: "DELETE",
-        })
-          .then((r) => r.json())
-          .then(() => handleDeleteNote(notes))
-      }
+  function handleDeleteClick(notes) {
+    fetch(`http://localhost:3000/notes/${notes.id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => handleDeleteNote(notes))
+  }
 
-      function handleSubmit(e) {
-        e.preventDefault();
-        const notesData = {
-          description: description,
-          name: name,
-        };
-        fetch("http://localhost:3000/notes", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(notesData),
+  function handleSubmit(e) {
+    e.preventDefault();
+    const notesData = {
+      description: description,
+      name: name,
+    };
+    fetch("http://localhost:3000/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(notesData),
+      })
+        .then((r) => r.json())
+        .then((newNotes) => {
+          const allNotesWithNew = [...notes, newNotes]
+          setNotes(allNotesWithNew)
         })
-          .then((r) => r.json())
-          .then((newNotes) => {
-            console.log(newNotes)
-            const allNotesWithNew = [...notes, newNotes]
-            setNotes(allNotesWithNew)
-          })
-        }
+  }
 
-    return (
-        <main>
-            <ul className="notesClass">
-            {allNotes}
-             <form onSubmit={handleSubmit}>
-                <label htmlFor="name-input">Name:</label>
-                 <input id="name-input" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-                 <label htmlFor="description-input">Description:</label>
-                <input id="description-input" type="text" value={description} onChange={(e) => setDescription(e.target.value)}/>
-                <input type="submit" />
-            </form>
-            </ul>
-         </main>
-          );
-        }
+  return (
+    <main>
+      <div className="notesTitle">Current Notes</div>
+      <ul className="notesList">
+        {allNotes}
+      </ul>
+        <div className="noteSubmission">
+        <div className="noteFormTitle">Add A New Note:</div>
+        <form className="noteForm" onSubmit={handleSubmit}>
+        <div className="noteNameInput">
+          <label htmlFor="name-input">Name:</label>
+            <input id="name-input" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+          </div>
+          <div className="noteDescription">
+          <label htmlFor="description-input">Description:</label>
+            <textarea id="description-input" type="text" value={description} onChange={(e) => setDescription(e.target.value)}/>
+            </div>
+            <input type="submit" className="noteSubmit"/>
+        </form>
+        </div>
+    </main>
+  );
+}
 
 export default NoteList;
