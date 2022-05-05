721ad7e84a7cce52f69f1f2a125d987c8de98af4
Emily <emilybribragg@gmail.com>
2022-05-05 15:10:56 -0400

    Final updates and code cleanup

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,18 +6,18 @@ import Nav from "react-bootstrap/Nav";
 
 function NavBar() {
   return (
-  <div className="navigation">
-    <Navbar bg="light" variant="light" className="navBar">
-      <div className="navContainer">
-      <div className="title">Noted.</div>
-      <Nav className="me-auto">
-        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-        <Nav.Link as={NavLink} to="/reminderlist">Reminders</Nav.Link>
-        <Nav.Link as={NavLink} to="/notelist">Notes</Nav.Link>
-      </Nav>
+    <div className="navigation">
+      <Navbar bg="light" variant="light" className="navBar">
+        <div className="navContainer">
+         <div className="title">Noted.</div>
+         <Nav className="me-auto">
+          <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/reminderlist">Reminders</Nav.Link>
+          <Nav.Link as={NavLink} to="/notelist">Notes</Nav.Link>
+         </Nav>
         </div>
-    </Navbar>
-  </div>
+      </Navbar>
+    </div>
   )
 }
 
