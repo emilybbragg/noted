7dc1629ee981f1d8b34cf619f178494f60033180
Emily <emilybribragg@gmail.com>
2022-05-05 15:11:29 -0400

    Final updates and code cleanup

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -5,10 +5,10 @@ function Note( {note, handleDeleteClick} ) {
   return (
     <div className="noteItemContainer">
     <li className="noteItems">
-        <strong>{note.name}</strong>
-        <br></br>
-        <span>{note.description}</span>
-        <button className="noteDelete" class="gg-trash" onClick={() => handleDeleteClick(note)}></button>
+      <strong>{note.name}</strong>
+      <br></br>
+      <span>{note.description}</span>
+      <button className="noteDelete gg-trash" onClick={() => handleDeleteClick(note)}></button>
     </li>
     </div>
   );
